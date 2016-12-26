ff6e221de7d9f9033350ad31a9d6782fa2b72e96
Ziqing <zack.ziqing.guo@gmail.com>
2016-12-26 18:23:39 +0800

    work

--- app/src/main/java/com/example/guoziqing/testproject/MainActivity.java
+++ app/src/main/java/com/example/guoziqing/testproject/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-//comment!!!! here!!!!!!!!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
